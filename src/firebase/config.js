3fa9693f4b8319a59d2857caeaa8e2ca3b034e52
juninbr2000 <edson.gentil.junior@gmail.com>
2024-10-14 14:01:30 -0300

    ajustes de variaveis de desenvolvimento

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAU5zyvXAkBkQ2ZaOYV56GcH9hFlrdy6YI",
-  authDomain: "imoveisgentil-bef5b.firebaseapp.com",
-  projectId: "imoveisgentil-bef5b",
-  storageBucket: "imoveisgentil-bef5b.appspot.com",
-  messagingSenderId: "530596864954",
-  appId: "1:530596864954:web:fd9828e9fa44f44b4df383",
-  measurementId: "G-YZXGR9QT0L"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
